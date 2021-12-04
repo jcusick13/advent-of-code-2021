1ef5ea35e96f98fc716dd4b6308371767a552366
jcusick13 <jonathan.cusick09@gmail.com>
2021-12-04 08:51:50 -0500

    Refactor ahead of part two

--- 03.c
+++ 03.c
@@ -3,6 +3,9 @@
 #include <string.h>
 #include "inputs/03.h"
 
+#define N_BITS 5
+#define N_ROWS 12
+
 struct Counter {
     int zero, one;
 };
@@ -25,48 +28,41 @@ int binaryToDecimal(int binary[], int n) {
     return decimal;
 }
 
-int main() {
-    const unsigned char *s = input;
-    int n_rows = 1000;
-    int n_bits = 12;
-    int report[n_rows][n_bits];
-    int cell = 0;
-
-    // Read inputs into 2d array as integers
-    int row;
-    while (*s != '\0') {
-        if (*s == '\n') s++;
-        row = (int)floor(cell / n_bits);
-        report[row][cell % n_bits] = (*s - '0');
-        cell++;
-        s++;
-    }
+struct Counter * countBitFreqs(int report[N_ROWS][N_BITS]){
+    /* Create array of length `N_BITS` with a `Counter`
+     * struct in each cell
+     */
 
     // Establish array of `Counter`s with zeroed starting values
-    struct Counter bit_counts[n_bits];
-    for (int i = 0; i < n_bits; i++) {
+    static struct Counter bit_counts[N_BITS];
+    for (int i = 0; i < N_BITS; i++) {
         initEmptyCounter(&bit_counts[i]);
     }
 
     // Tally all input data per bit-column
-    for (int row = 0; row < n_rows; row++) {
-        for (int bit = 0; bit < n_bits; bit++) {
+    for (int row = 0; row < N_ROWS; row++) {
+        for (int bit = 0; bit < N_BITS; bit++) {
             if (report[row][bit] == 0) {
                 bit_counts[bit].zero += 1;
             }
             else if (report[row][bit] == 1) {
                 bit_counts[bit].one += 1;
             }
-            else {
-                return -1;
-            }
+            else continue;
         }
     }
 
+    return bit_counts;
+}
+
+int powerConsumption(int report[N_ROWS][N_BITS]) {
+    struct Counter *bit_counts;
+    bit_counts = countBitFreqs(report);
+
     // Build up gamma and epsilon as arrays
-    int binary_gamma[n_bits];
-    int binary_epsilon[n_bits];
-    for (int i = 0; i < n_bits; i++) {
+    int binary_gamma[N_BITS];
+    int binary_epsilon[N_BITS];
+    for (int i = 0; i < N_BITS; i++) {
         if (bit_counts[i].zero > bit_counts[i].one) {
             binary_gamma[i] = 0;
             binary_epsilon[i] = 1;
@@ -77,13 +73,30 @@ int main() {
         }
     }
 
-    int gamma = binaryToDecimal(binary_gamma, n_bits);
-    int epsilon = binaryToDecimal(binary_epsilon, n_bits);
+    int gamma = binaryToDecimal(binary_gamma, N_BITS);
+    int epsilon = binaryToDecimal(binary_epsilon, N_BITS);
     int power_consumption = gamma * epsilon;
 
-    printf("%d\n", gamma);
-    printf("%d\n", epsilon);
-    printf("%d\n", power_consumption);
+    return power_consumption;
+}
+
+int main() {
+    const unsigned char *s = input;
+    int report[N_ROWS][N_BITS];
+    int cell = 0;
+
+    // Read inputs into 2d array as integers
+    int row;
+    while (*s != '\0') {
+        if (*s == '\n') s++;
+        row = (int)floor(cell / N_BITS);
+        report[row][cell % N_BITS] = (*s - '0');
+        cell++;
+        s++;
+    }
+
+    int power_consumption = powerConsumption(report);
+    printf("Power consumption: %d\n", power_consumption);
 
     return 0;
 }
