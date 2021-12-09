b6c7cf3a8f1ec51eb03a4e98bf8679827b4e32a6
Jon Cusick <jonathan.cusick09@gmail.com>
2021-12-09 14:00:18 -0500

    Correct row check, thanks Jacob!

--- 04.c
+++ 04.c
@@ -26,7 +26,10 @@ bool boardFinished(BingoCell *board) {
         int marked_col_vals = 0;
         for (int cell = 0; cell < (SIZE * SIZE); cell++) {
             if (cell % SIZE != col) continue;
-            if (!board[cell].marked) break;
+            if (!board[cell].marked) {
+                marked_col_vals = 0;
+                break;
+            }
             marked_col_vals += 1;
 
             if (marked_col_vals == SIZE) {
@@ -38,9 +41,12 @@ bool boardFinished(BingoCell *board) {
     // Check for complete row
     for (int row = 0; row < SIZE; row++) {
         int marked_row_vals = 0;
-        for (int cell = 1; cell < ((SIZE * SIZE) + 1); cell++) {
-            if (cell < row || cell > (row * SIZE)) continue;
-            if (!board[cell - 1].marked) break;
+        for (int cell = 0; cell < (SIZE * SIZE); cell++) {
+            if (cell  < (row * SIZE) || cell >= ((row + 1) * SIZE)) continue;
+            if (!board[cell].marked) {
+                marked_row_vals = 0;
+                break;
+            }
             marked_row_vals += 1;
 
             if (marked_row_vals == SIZE) {
@@ -70,7 +76,6 @@ int main() {
             s++;
             n++;
         }
-        printf("read in numbers (n=%d)\n", n);
 
         // Skip down to board descriptions
         s++;
@@ -101,37 +106,26 @@ int main() {
 
         break;
     }
-    printf("read in boards\n");
 
 
     bool found_finished_board = false;
     int finished_board, winning_number;
     for (int i = 0; i < n; i++) {
-        printf("\nCalled %d\n", drawn_numbers[i]);
         for (int board_i = 0; board_i < N_BOARDS; board_i++) {
             markBoard(boards[board_i], drawn_numbers[i]);
 
             if (boardFinished(boards[board_i])) {
                 found_finished_board = true;
                 finished_board = board_i;
-                for (int z=0; z < 25; z++){
-                    printf("%d (%d)\n", boards[board_i][z].number, boards[board_i][z].marked);
-                }
                 break;
             }
         }
-        /*
-        for (int i=0; i < 25; i++){
-            printf("%d (%d)\n", boards[2][i].number, boards[2][i].marked);
-        }
-        */
 
         if (found_finished_board) {
             winning_number = drawn_numbers[i];
             break;
         }
     }
-    printf("found winning board\n");
 
     // Sum unmarked numbers of selected board
     int unmarked = 0;
@@ -139,7 +133,6 @@ int main() {
         if (boards[finished_board][cell].marked) continue;
         unmarked += boards[finished_board][cell].number;
     }
-    printf("summing unmarked\n");
 
     printf("\n%d", unmarked * winning_number);
 
